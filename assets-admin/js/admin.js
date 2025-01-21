968319749cf59b3dee7ffc82f25f2c60e7be3aa4
aminmaharramzade <maharramzadeamin@gmail.com>
2025-01-21 13:24:53 +0400

    update admin page

---
+++ assets-admin/js/admin.js
@@ -0,0 +1,26 @@
+const passwordEye = document.querySelector(`#passwordEye`);
+const passwordInput = document.querySelector(`#inputPassword`);
+const nameInput = document.querySelector(`#inputName`);
+const loginBtn = document.querySelector(`#loginBtn`);
+
+// password private
+passwordEye.addEventListener("click", function () {
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+  } else {
+    passwordInput.type = "password";
+  }
+});
+
+// required login
+loginBtn.addEventListener("click", function () {
+  if (nameInput.value === "") {
+    nameInput.setAttribute("placeholder", "Required");
+  }
+  if (passwordInput.value === "") {
+    passwordInput.setAttribute("placeholder", "Required");
+  }
+  if (nameInput.value !== "" && passwordInput.value !== "") {
+    window.location.href = "../pages/dashboard.html";
+  }
+});
