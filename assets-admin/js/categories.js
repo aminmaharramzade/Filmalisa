d4b0ba2747299e6d6319fe34b8e19379ee3baa44
SurkhayliShukur <shukursurkhayli000@gmail.com>
2025-01-25 17:06:57 +0400

    categories api

--- assets-admin/js/categories.js
+++ assets-admin/js/categories.js
@@ -3,10 +3,9 @@ const submitButton = document.querySelector('.custom-btn');
 const modal = document.querySelector('#exampleModal');
 
 const baseURL = "https://api.sarkhanrahimli.dev/api/filmalisa/admin";
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInN1YiI6MywiaWF0IjoxNzM3NjE4MzExLCJleHAiOjE3Njg3MjIzMTF9.D764-UeEH-tpz_lCtnPXi2ZcaydOOjh16-4SchtnFX4";
-let editMode = false;
-let editRow = null;
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInN1YiI6MywiaWF0IjoxNzM3NzkyNzgzLCJleHAiOjE3Njg4OTY3ODN9.xd6XkHnR3hiWw8rlX-YBuQjzLxTYtoVmeS4zkl04rfc";
 
+// GET 
 async function fetchData(endpoint) {
   try {
     const response = await fetch(`${baseURL}/${endpoint}`, {
@@ -47,43 +46,9 @@ function addCategoryToTable(category) {
   `;
   categoryTableBody.appendChild(row);
 
-  // Add event listener to the delete button
-  row.querySelector('.delete-btn').addEventListener('click', function() {
-    row.remove();
-  });
 
-  // Add event listener to the edit button
-  row.querySelector('.edit-btn').addEventListener('click', function() {
-    editMode = true;
-    editRow = row;
-    document.querySelector('#title').value = category.name;
-    const modalInstance = new bootstrap.Modal(modal);
-    modalInstance.show();
-  });
+ 
+
 }
 
-submitButton.addEventListener('click', function(event) {
-  event.preventDefault();
-  
-  const name = document.querySelector('#title').value;
-  
-  if (editMode) {
-    editRow.querySelector('td:nth-child(2)').textContent = name;
-    editMode = false;
-    editRow = null;
-  } else {
-    const newCategory = {
-      id: categoryTableBody.children.length + 1,
-      name: name
-    };
-    addCategoryToTable(newCategory);
-  }
-
-  const modalInstance = bootstrap.Modal.getInstance(modal);
-  modalInstance.hide();
-
-  // Reset the modal values
-  document.querySelector('#title').value = '';
-});
-
-// fetchData('categories'); // Commented out to prevent initial display
+fetchData('categories'); 
