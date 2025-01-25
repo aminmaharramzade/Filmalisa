1c80a62bda34e63b4a0f4c88b546b3c42035f7e1
aminmaharramzade <maharramzadeamin@gmail.com>
2025-01-26 02:55:18 +0400

    update favourite api

--- assets-client/js/details.js
+++ assets-client/js/details.js
@@ -1,33 +1,5 @@
-const baseURL = "https://api.sarkhanrahimli.dev/api/filmalisa/admin";
+const baseURL = "https://api.sarkhanrahimli.dev/api/filmalisa";
 const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInN1YiI6MywiaWF0IjoxNzM3NTc0OTIyLCJleHAiOjE3Njg2Nzg5MjJ9.xM7OU1oOOgk-SicNULAy_ogKRoNy1aiF3SBwVFokyyg`;
-
-function updateSearchPlusIcon(isFavourite) {
-  const searchPlusIcon = document.querySelector(".search-plus img");
-  if (isFavourite) {
-    searchPlusIcon.style.backgroundColor = "#12cb5c80";
-    searchPlusIcon.style.rotate = "45deg";
-  } else {
-    searchPlusIcon.style.backgroundColor = "";
-    searchPlusIcon.style.rotate = "";
-  }
-}
-
-function toggleFavourite(movie) {
-  let favourites = JSON.parse(localStorage.getItem("favourites")) || [];
-  const movieIndex = favourites.findIndex((fav) => fav.id === movie.id);
-
-  if (movieIndex > -1) {
-    favourites.splice(movieIndex, 1);
-    showModal("Movie removed from favourites!");
-    document.querySelector(".modal").style.backgroundColor = "rgb(156, 51, 51)";
-  } else {
-    favourites.push(movie);
-    showModal("Movie added to favourites!");
-  }
-
-  localStorage.setItem("favourites", JSON.stringify(favourites));
-  updateSearchPlusIcon(movieIndex === -1);
-}
 
 async function fetchMovieDetails(id) {
   try {
@@ -83,23 +55,47 @@ async function fetchMovieDetails(id) {
       `;
       actorsArea.appendChild(actorCard);
     });
-
-    const favourites = JSON.parse(localStorage.getItem("favourites")) || [];
-    const isFavourite = favourites.some((fav) => fav.id === data.data.id);
-    updateSearchPlusIcon(isFavourite);
-
-    document.querySelector(".search-plus img").addEventListener("click", () => {
-      toggleFavourite(data.data);
-    });
-
   } catch (error) {
     console.error("Error:", error);
   }
 }
 
+const urlParams = new URLSearchParams(window.location.search);
+const movieId = urlParams.get("id");
+if (movieId) {
+  fetchMovieDetails(movieId);
+}
+
+const favBtn = document.querySelector(".search-plus");
+favBtn.addEventListener("click", async function () {
+  try {
+    const response = await fetch(`${baseURL}/movie/${movieId}/favorite`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ movie_id: movieId }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const data = await response.json();
+    showModal(data.message);
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+});
+
 function showModal(message) {
   const modal = document.createElement("div");
   modal.classList.add("modal");
+  if (message.includes("removed")) {
+    modal.style.backgroundColor = "rgba(203, 66, 66, 0.54)";
+  }
   modal.innerHTML = `
     <div class="modal-content">
       <p>${message}</p>
@@ -110,10 +106,4 @@ function showModal(message) {
   setTimeout(() => {
     modal.remove();
   }, 1000);
-}
-
-const urlParams = new URLSearchParams(window.location.search);
-const movieId = urlParams.get("id");
-if (movieId) {
-  fetchMovieDetails(movieId);
 }
--- assets-client/js/favourite.js
+++ assets-client/js/favourite.js
@@ -1,32 +1,73 @@
-const baseURL = "https://api.sarkhanrahimli.dev/api/filmalisa/admin";
-const token = localStorage.getItem("accessToken");
+const baseURL = "https://api.sarkhanrahimli.dev/api/filmalisa/movies";
+const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInN1YiI6MywiaWF0IjoxNzM3NTc0OTIyLCJleHAiOjE3Njg2Nzg5MjJ9.xM7OU1oOOgk-SicNULAy_ogKRoNy1aiF3SBwVFokyyg`;
 
-function loadFavourites() {
-  const favourites = JSON.parse(localStorage.getItem("favourites")) || [];
-  const favouriteArea = document.querySelector("#favouriteArea");
-  favouriteArea.innerHTML = "";
-
-  favourites.forEach((movie) => {
-    const card = document.createElement("div");
-    card.classList.add("favourite-card");
-
-    card.addEventListener("click", () => {
-      window.location.href = `../pages/details.html?id=${movie.id}`;
+async function fetchFavourites(endpoint) {
+  try {
+    const response = await fetch(`${baseURL}/${endpoint}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
     });
 
-    card.innerHTML = `
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data.data);
+    const favouriteArea = document.querySelector("#favouriteArea");
+    favouriteArea.innerHTML = "";
+
+    data.data.forEach((movie) => {
+      const card = document.createElement("div");
+      card.classList.add("favourite-card");
+
+      card.addEventListener("click", () => {
+        window.location.href = `../pages/details.html?id=${movie.id}`;
+      });
+
+      const rating = Math.round(movie.imdb);
+      let starIcon = "";
+      let filledStars = 0;
+
+      if (rating >= 1 && rating <= 3) {
+        filledStars = 2;
+      } else if (rating >= 4 && rating <= 5) {
+        filledStars = 3;
+      } else if (rating >= 6 && rating <= 7) {
+        filledStars = 4;
+      } else if (rating >= 8 && rating <= 10) {
+        filledStars = 5;
+      }
+
+      for (let i = 0; i < 5; i++) {
+        if (i < filledStars) {
+          starIcon +=
+            '<li><img src="../assets/icons/star-raiting.svg" alt="filled star" /></li>';
+        }
+      }
+
+      card.innerHTML = `
       <div class="gradient-overlay"></div>
       <img
         src="${movie.cover_url}"
         alt="${movie.title}"
         class="background-image"
       />
-      <h4>${movie.category.name}</h4>
+      <ul class="star-rating">
+        ${starIcon}
+      </ul>
+
       <h1>${movie.title}</h1>
     `;
 
-    favouriteArea.appendChild(card);
-  });
+      favouriteArea.appendChild(card);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-document.addEventListener("DOMContentLoaded", loadFavourites);
+fetchFavourites("favorites");
