fc98de57f4ca2ef59305d8e4d938080499054c9c
aminmaharramzade <maharramzadeamin@gmail.com>
2025-01-27 03:36:23 +0400

    create api register

--- assets-client/js/register.js
+++ assets-client/js/register.js
@@ -1,5 +1,5 @@
 const passwordEye = document.querySelector(`#passwordEye`);
-const nameEye = document.querySelector(`#nameEye`)
+const nameEye = document.querySelector(`#nameEye`);
 const passwordInput = document.querySelector(`#passwordInput`);
 const emailInput = document.querySelector(`#emailInput`);
 const nameInput = document.querySelector(`#nameInput`);
@@ -16,15 +16,41 @@ passwordEye.addEventListener("click", function () {
 
 //fullname private
 nameEye.addEventListener("click", function () {
-    if (nameInput.type === "text") {
-      nameInput.type = "password";
-    } else {
-      nameInput.type = "text";
-    }
-  });
+  if (nameInput.type === "text") {
+    nameInput.type = "password";
+  } else {
+    nameInput.type = "text";
+  }
+});
 
-// required login
-registerBtn.addEventListener("click", function () {
+const baseURL = "https://api.sarkhanrahimli.dev/api/filmalisa/auth";
+// POST
+async function postData(endpoint, password, name, email) {
+  try {
+    const response = await fetch(`${baseURL}/${endpoint}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        password: password,
+        full_name: name,
+        email: email,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+registerBtn.addEventListener("click", (event) => {
   if (nameInput.value === "") {
     nameInput.setAttribute("placeholder", "Required");
   }
@@ -34,7 +60,21 @@ registerBtn.addEventListener("click", function () {
   if (passwordInput.value === "") {
     passwordInput.setAttribute("placeholder", "Required");
   }
-  if (nameInput.value !== "" && emailInput.value !== "" && passwordInput.value !== "") {
-    window.location.href = "../../index.html";
+  if (
+    !nameInput.value == "" &&
+    !emailInput.value == "" &&
+    !passwordInput.value == ""
+  ) {
+    registerBtn.style.backgroundColor = "rgba(13, 136, 13, 0.542)";
+    setTimeout(() => {
+      window.location.href = "../pages/login.html";
+    }, 2000);
+
+    event.preventDefault();
+    postData("signup", passwordInput.value, nameInput.value, emailInput.value);
   }
+
+  nameInput.value = "";
+  emailInput.value = "";
+  passwordInput.value = "";
 });
