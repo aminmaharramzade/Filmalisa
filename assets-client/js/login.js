556cad10fecc7ade0017da6f96e21bda99a669f6
aminmaharramzade <maharramzadeamin@gmail.com>
2025-01-27 03:58:47 +0400

    create api login

--- assets-client/js/login.js
+++ assets-client/js/login.js
@@ -3,6 +3,8 @@ const passwordInput = document.querySelector(`#passwordInput`);
 const emailInput = document.querySelector(`#emailInput`);
 const loginBtn = document.querySelector(`#loginBtn`);
 
+const baseURL = "https://api.sarkhanrahimli.dev/api/filmalisa";
+
 // password private
 passwordEye.addEventListener("click", function () {
   if (passwordInput.type === "password") {
@@ -13,7 +15,8 @@ passwordEye.addEventListener("click", function () {
 });
 
 // required login
-loginBtn.addEventListener("click", function () {
+loginBtn.addEventListener("click", async function (event) {
+  event.preventDefault();
   if (emailInput.value === "") {
     emailInput.setAttribute("placeholder", "Required");
   }
@@ -21,6 +24,59 @@ loginBtn.addEventListener("click", function () {
     passwordInput.setAttribute("placeholder", "Required");
   }
   if (emailInput.value !== "" && passwordInput.value !== "") {
-    window.location.href = "../pages/home.html";
+    try {
+      const response = await fetch(`${baseURL}/auth/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: emailInput.value,
+          password: passwordInput.value,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+
+      const token = data.data.tokens.access_token;
+      localStorage.setItem("accessToken", token);
+
+      const usersResponse = await fetch(`${baseURL}/admin/users`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!usersResponse.ok) {
+        throw new Error(`Error: ${usersResponse.status}`);
+      }
+
+      const usersData = await usersResponse.json();
+      const user = usersData.data.find(
+        (user) => user.email === emailInput.value
+      );
+
+      if (user) {
+        window.location.href = "../pages/home.html";
+      } else {
+        emailInput.value = "";
+        passwordInput.value = "";
+        emailInput.setAttribute("placeholder", "Invalid email");
+        passwordInput.setAttribute("placeholder", "Invalid password");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      emailInput.value = "";
+      passwordInput.value = "";
+      emailInput.setAttribute("placeholder", "Invalid email");
+      passwordInput.setAttribute("placeholder", "Invalid password");
+    }
   }
 });
