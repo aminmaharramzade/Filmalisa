a3e2a93f568cd36dc3dd1a111cb5dea434f37725
aminmaharramzade <maharramzadeamin@gmail.com>
2025-01-27 02:49:24 +0400

    create api contact

--- assets-client/js/index.js
+++ assets-client/js/index.js
@@ -10,3 +10,60 @@ plusBtns.forEach((btn, index) => {
     }
   });
 });
+
+const emailInput = document.querySelector(`#emailInput`);
+const nameInput = document.querySelector(`#nameInput`);
+const reasonInput = document.querySelector(`#reasonInput`);
+const submitBtn = document.querySelector("#submitBtn");
+
+const baseURL = "https://api.sarkhanrahimli.dev/api/filmalisa";
+// POST
+async function postData(endpoint, name, email, reason) {
+  try {
+    const response = await fetch(`${baseURL}/${endpoint}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        full_name: name,
+        email: email,
+        reason: reason,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+submitBtn.addEventListener("click", (event) => {
+  if (nameInput.value === "") {
+    nameInput.setAttribute("placeholder", "Required");
+  }
+  if (emailInput.value === "") {
+    emailInput.setAttribute("placeholder", "Required");
+  }
+  if (reasonInput.value === "") {
+    reasonInput.setAttribute("placeholder", "Required");
+  }
+  if (
+    !nameInput.value == "" &&
+    !emailInput.value == "" &&
+    !reasonInput.value == ""
+  ) {
+    submitBtn.style.backgroundColor = "rgba(13, 136, 13, 0.542)";
+    event.preventDefault();
+    postData("contact", nameInput.value, emailInput.value, reasonInput.value);
+  }
+
+  nameInput.value = "";
+  emailInput.value = "";
+  reasonInput.value = "";
+});
