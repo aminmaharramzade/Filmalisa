d4a7d140989a7dd27f1ba8f1702ae22a3373d982
aminmaharramzade <maharramzadeamin@gmail.com>
2025-01-26 05:04:05 +0400

    update details page

--- assets-client/js/details.js
+++ assets-client/js/details.js
@@ -107,3 +107,79 @@ function showModal(message) {
     modal.remove();
   }, 1000);
 }
+
+
+async function fetchMovies(endpoint){
+  try {
+    const response = await fetch(`${baseURL}/admin/${endpoint}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data.data);
+
+    const comedyCardArea = document.querySelector(".comedy-card-area");
+    comedyCardArea.innerHTML = "";
+
+    data.data.forEach((movie) => {
+      const rating = Math.round(movie.imdb);
+      let starIcon = "";
+      let filledStars = 0;
+
+      if (rating >= 1 && rating <= 3) {
+        filledStars = 2;
+      } else if (rating >= 4 && rating <= 5) {
+        filledStars = 3;
+      } else if (rating >= 6 && rating <= 7) {
+        filledStars = 4;
+      } else if (rating >= 8 && rating <= 10) {
+        filledStars = 5;
+      }
+
+      for (let i = 0; i < 5; i++) {
+        if (i < filledStars) {
+          starIcon +=
+            '<li><img src="../assets/icons/star-raiting.svg" alt="filled star" /></li>';
+        }
+      }
+      const card = document.createElement("div");
+      card.classList.add("comedy-card");
+
+      card.addEventListener("click", () => {
+        window.location.href = `../pages/details.html?id=${movie.id}`;
+      });
+
+      card.innerHTML = `
+      <div class="gradient-overlay"></div>
+      <img
+        src="${movie.cover_url}"
+        alt="${movie.title}"
+        class="background-image"
+      />
+      <div class="comedy-card-text">
+        <div class=bg-category>
+          <h4>${movie.category.name}</h4>
+        </div>
+        <ul class="star-rating">
+          ${starIcon}
+        </ul>
+        <h1>${movie.title}</h1>
+      </div>
+    `;
+        comedyCardArea.appendChild(card);
+
+    });
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+fetchMovies("movies");
