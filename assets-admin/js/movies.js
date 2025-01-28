265879d86674b9304edb0ba5bb35407cab27f70e
SurkhayliShukur <shukursurkhayli000@gmail.com>
2025-01-28 11:41:40 +0400

    table-category

--- assets-admin/js/movies.js
+++ assets-admin/js/movies.js
@@ -5,9 +5,10 @@ const modal = document.querySelector('#exampleModal');
 
 const baseURL = "https://api.sarkhanrahimli.dev/api/filmalisa/admin";
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInN1YiI6MywiaWF0IjoxNzM3OTg4OTIzLCJleHAiOjE3NjkwOTI5MjN9.9Hztt689ECGP3QyArKno3NNKfX2Uu8HlFe1s132RzCc";
-let editMode = false;
-let editRow = null;
 
+
+
+// GET Movies
 async function fetchData(endpoint) {
   try {
     const response = await fetch(`${baseURL}/${endpoint}`, {
@@ -32,6 +33,27 @@ async function fetchData(endpoint) {
     console.error("Error:", error);
   }
 }
+async function fetchCategories(endpoint) {
+  try {
+    const response = await fetch(`${baseURL}/${endpoint}`, {
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data.data);
+    categoriesList = data.data;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
 
 function addMovieToTable(movie) {
   const row = document.createElement('tr');
@@ -39,7 +61,7 @@ function addMovieToTable(movie) {
     <th scope="row">${movie.id}</th>
     <td>${movie.title}</td>
     <td>${movie.overview.slice(0,30)}</td>
-    <td>${movie.category}</td>
+    <td>${movie.category.name}</td>
     <td>${movie.imdb}</td>
     <td>
       <button class="btn btn-primary edit-btn">
@@ -51,62 +73,9 @@ function addMovieToTable(movie) {
     </td>
   `;
   movieTableBody.appendChild(row);
-
-  row.querySelector('.delete-btn').addEventListener('click', function() {
-    row.remove();
-  });
-
-
-  row.querySelector('.edit-btn').addEventListener('click', function() {
-    editMode = true;
-    editRow = row;
-    document.querySelector('#title').value = movie.title;
-    document.querySelector('#overview').value = movie.overview;
-    document.querySelector('#imdb').value = movie.imdb;
-    category.value = movie.category;
-    const modalInstance = new bootstrap.Modal(modal);
-    modalInstance.show();
-  });
 }
 
-submitButton.addEventListener('click', function(event) {
-  event.preventDefault();
-  
-  const title = document.querySelector('#title').value;
-  const overview = document.querySelector('#overview').value;
-  const categoryValue = category.value;
-  const imdb = document.querySelector('#imdb').value;
-  
-  if (editMode) {
-    editRow.querySelector('td:nth-child(2)').textContent = title;
-    editRow.querySelector('td:nth-child(3)').textContent = overview;
-    editRow.querySelector('td:nth-child(4)').textContent = categoryValue;
-    editRow.querySelector('td:nth-child(5)').textContent = imdb;
-    editMode = false;
-    editRow = null;
-  } else {
-    const newMovie = {
-      id: movieTableBody.children.length + 1,
-      title: title,
-      overview: overview,
-      category: categoryValue,
-      imdb: imdb
-    };
-    addMovieToTable(newMovie);
-  }
-
-  const modalInstance = bootstrap.Modal.getInstance(modal);
-  modalInstance.hide();
-
-  // Reset the modal values
-  document.querySelector('#title').value = '';
-  document.querySelector('#overview').value = '';
-  document.querySelector('#coverUrl').value = '';
-  document.querySelector('#trailerUrl').value = '';
-  document.querySelector('#watchUrl').value = '';
-  document.querySelector('#imdb').value = '';
-  document.querySelector('#runtime').value = '';
-  category.selectedIndex = 0;
-});
-
-fetchData('movies');
+(async function init() {
+  await fetchCategories('categories'); 
+  fetchData('movies'); 
+})();
