e466cbffc6638a9d2e619a3473505f69c1e2363a
SurkhayliShukur <shukursurkhayli000@gmail.com>
2025-01-29 19:07:43 +0400

    movies-category

--- assets-admin/js/movies.js
+++ assets-admin/js/movies.js
@@ -3,11 +3,29 @@ const movieTableBody = document.querySelector('.movie-table tbody');
 const submitButton = document.querySelector('.submit button');
 const modal = document.querySelector('#exampleModal');
 const pagination = document.querySelector('.pagination');
+const moviesForm = document.querySelector(".movies-form");
 let currentPage = 1;
 const rowsPerPage = 10;
 
 const baseURL = "https://api.sarkhanrahimli.dev/api/filmalisa/admin";
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInN1YiI6MywiaWF0IjoxNzM3OTg4OTIzLCJleHAiOjE3NjkwOTI5MjN9.9Hztt689ECGP3QyArKno3NNKfX2Uu8HlFe1s132RzCc";
+
+function showNotification(type, message) {
+  toastr.options = {
+    positionClass: "toast-top-right",
+    timeOut: 3000, 
+  };
+
+  if (type === "success") {
+    toastr.success(message);
+  } else if (type === "error") {
+    toastr.error(message);
+  } else if (type === "info") {
+    toastr.info(message);
+  } else if (type === "warning") {
+    toastr.warning(message);
+  }
+}
 
 
 
@@ -49,12 +67,80 @@ async function fetchCategories(endpoint) {
     }
 
     const data = await response.json();
-    console.log(data.data);
-    categoriesList = data.data;
+    category.innerHTML = '<option selected disabled>Choose category</option>';
+    
+    data.data.forEach(cat => {
+      const option = document.createElement("option");
+      option.value = cat.id;
+      option.textContent = cat.name;
+      category.appendChild(option);
+    });
   } catch (error) {
     console.error("Error:", error);
   }
+
+  
 }
+// POST Movies
+document.querySelector(".movies-form").addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const movieTitle = document.getElementById("title").value.trim();
+  const overview = document.getElementById("overview").value.trim();
+  const coverUrl = document.getElementById("coverUrl").value.trim();
+  const trailerUrl = document.getElementById("trailerUrl").value.trim();
+  const watchUrl = document.getElementById("watchUrl").value.trim();
+  const imdb = document.getElementById("imdb").value.trim();
+  const runtime = document.getElementById("runtime").value.trim();
+  const categoryId = document.getElementById("category").value;
+  const adult = document.getElementById("adult").checked;
+
+  // if (!movieTitle || !overview || !coverUrl || !trailerUrl || !watchUrl || !imdb || !runtime || !categoryId) {
+  //   showNotification("error", "Please fill in all required fields!");
+  //   return;
+  // }
+
+  try {
+    const response = await fetch(`${baseURL}/movie`, {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: movieTitle,
+        overview: overview,
+        coverUrl: coverUrl,
+        trailerUrl: trailerUrl,
+        watchUrl: watchUrl,
+        imdb: imdb,
+        runtime: Number(runtime), 
+        adult: adult, // Buraya əlavə etdik
+      }),
+      
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      displayMovies(data.data, movieTableBody, rowsPerPage, currentPage);
+      setupPagination(data.data, pagination, rowsPerPage);
+      showNotification("success", "Movie successfully added!");
+      
+      // Formu təmizlə
+      document.querySelector(".movies-form").reset();
+
+      const modal = bootstrap.Modal.getInstance(document.getElementById("exampleModal"));
+      modal.hide();
+    } else {
+      showNotification("error", "Error: " + response.status);
+    }
+  } catch (error) {
+    showNotification("error", error.message);
+  }
+});
+
+
+
 
 function displayMovies(movies, wrapper, rowsPerPage, page) {
   wrapper.innerHTML = "";
